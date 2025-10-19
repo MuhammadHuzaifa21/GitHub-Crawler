f66752628ff4e3141608b58b318053980424067c
MuhammadHuzaifa21 <m.huzaifa2k@gmail.com>
2025-10-19 17:37:28 +0500

    removed dotenv

--- github_to_postgres.py
+++ github_to_postgres.py
@@ -2,9 +2,9 @@ import requests
 import psycopg2
 import time
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 # --- GitHub API Config ---
 url = "https://api.github.com/graphql"
